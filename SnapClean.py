65384e78977f4ed0cad0392ff945328bd9a52a4f
System Administrator <root@Neptune.local>
2016-08-27 23:05:56 -0500

    Filtering working, includes owner-id

--- SnapClean.py
+++ SnapClean.py
@@ -2,19 +2,25 @@
 
 import boto3
 import datetime
+import pytz
+import argparse
+import logging
+import logging.handlers
 
 
 
 class SnapClean(object):
-    def __init__(self, region, retentionTime, tagKey, tagValue, logLevel, dryRunFlag):
+    def __init__(self, region, retentionTime, tagKey, tagValue, account, logLevel, dryRunFlag):
         self.region = region
         self.retentionTime = retentionTime
         self.tagKey = tagKey
         self.tagValue = tagValue
+        self.account = account
         self.logLevel = logLevel
         self.dryRunFlag = dryRunFlag
 
-        self.currDateTime = datetime.datetime.now()
+        #self.currDateTime = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+        self.currDateTime = datetime.datetime.now(pytz.utc)
         self.expirationDateTime = self.currDateTime - datetime.timedelta(days=self.retentionTime)
 
 
@@ -65,19 +71,33 @@ class SnapClean(object):
 
         # Get the EC2 Service Resource
         ec2ServiceResourceAPI = boto3.resource('ec2', region_name=self.region)
-        snapshotAPI = ec2ServiceResourceAPI.Snapshot('id')
+        snapshotAPI = ec2ServiceResourceAPI.snapshots
 
 
         # Step 1: Get all snapshots matching the Tag/Value
+        # targetFilter = [
+        #     {
+        #         'Name': 'status',
+        #         'Values': ['completed']
+        #     },
+        #     {
+        #         'Name': 'tag:' + self.tagKey,
+        #         'Values': [self.tagValue]
+        #     },
+        # ]
         targetFilter = [
             {
-                'Name': 'state',
+                'Name': 'status',
                 'Values': ['completed']
+            },
+            {
+                'Name': 'owner-id',
+                'Values': [self.account]
             },
             {
                 'Name': 'tag:' + self.tagKey,
                 'Values': [self.tagValue]
-            },
+            }
         ]
 
         snapshot_iterator=iter([])  # empty iter for scope purposes
@@ -89,14 +109,22 @@ class SnapClean(object):
             self.logger.error('Exception filtering snapshots %s' + str(e))
 
         # Step #2: Collect all snapshots older than retentionTime
+        TOTAL_SNAPSHOTS_FOUND='totalSnapshotsFound'
+        EXPIRED_SNAPSHOTS_FOUND='expiredSnapshotsFound'
+        EXCEPETIONS_ENCOUNTERED='exceptionsEncountered'
+
+        results = {TOTAL_SNAPSHOTS_FOUND: 0, EXPIRED_SNAPSHOTS_FOUND: 0, EXCEPETIONS_ENCOUNTERED: 0}
         expiredSnapshots = []
         for snapshot in snapshot_iterator:
-            assert isinstance(snapshot.start_time, datetime), '%r is not a datetime' % snapshot.start_time
+            assert isinstance(snapshot.start_time, datetime.datetime), '%r is not a datetime' % snapshot.start_time
+            results[TOTAL_SNAPSHOTS_FOUND] = results[TOTAL_SNAPSHOTS_FOUND] + 1
             self.logger.debug('Found snapshot matching tag: %s', snapshot.snapshot_id)
             snapshotStartDateTime = snapshot.start_time
             # if snapshot start_time is older than the expiration retention date, add it to the expired list
             if(snapshotStartDateTime < self.expirationDateTime):
                 expiredSnapshots.append(snapshot)
+
+        results[EXPIRED_SNAPSHOTS_FOUND] = len(expiredSnapshots)
 
         # Step #3: Delete snapshots in Collection, unless dryRunFlag is set
         if(self.dryRunFlag == False ):
@@ -106,8 +134,14 @@ class SnapClean(object):
                     snapshot.delete()
                 except Exception as e:
                     self.logger.error('Exception deleting snapshot %s, %s' % (snapshot.snapshot_id, str(e)))
-        else
+                    results[EXPIRED_SNAPSHOTS_FOUND] = results[EXPIRED_SNAPSHOTS_FOUND] + 1
+        else:
             self.logger.warning('Dryrun flag is set, no snapshots will be deleted')
+
+        self.logger.info('Total Snapshots inspected %s', results[TOTAL_SNAPSHOTS_FOUND])
+        self.logger.info('Expired Snapshots %s', results[EXPIRED_SNAPSHOTS_FOUND])
+        self.logger.info('Exceptions Encountered %s', results[EXCEPETIONS_ENCOUNTERED])
+
 
 
 
@@ -120,6 +154,8 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--time', help='Age in days after which snapshot is deleted', required=True)
     parser.add_argument('-k', '--tagKey', help='The name of the Tag Key used for snapshot searches', required=True)
     parser.add_argument('-v', '--tagValue', help='The Tag Value used to match snapshot searches', required=True)
+    parser.add_argument('-a', '--account', help='AWS account number', required=True)
+
     parser.add_argument('-d', '--dryrun', action='count', help='Run but take no Action', required=False)
     parser.add_argument('-l', '--loglevel', choices=['critical', 'error', 'warning', 'info', 'debug', 'notset'],
                         help='The level to record log messages to the logfile', required=False)
@@ -139,5 +175,5 @@ if __name__ == "__main__":
         dryRun = False
 
     # Launch SnapClean
-    snapCleanMain = SnapClean(args.region, args.time, args.tagKey, args.tagValue, loglevel, dryRun)
+    snapCleanMain = SnapClean(args.region, int(args.time), args.tagKey, args.tagValue, args.account, loglevel, dryRun)
     snapCleanMain.execute()
